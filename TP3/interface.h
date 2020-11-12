7689b1ef58db6f284f0c00cf788aad9ed90fd4af
Gustavo Mendes <gustavo.sm00@gmail.com>
2020-11-12 13:54:45 +0000

    merged

--- TP3/interface.h
+++
@@ -1,9 +0,0 @@
-#include "macros.h"
-#include "application.h"
-
-typedef struct{
-    char *port;  /*Path da porta de série passado pelo user*/
-    int fileDescriptor;/*Descritor correspondente à porta série*/
-    int status;/*TRANSMITTER | RECEIVER*/
-    char *path;
-}applicationLayer;
